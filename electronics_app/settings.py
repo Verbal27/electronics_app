a51d77923486226a1fddd9005d58df606dad095b
Verbal27 <43434781+Verbal27@users.noreply.github.com>
2025-11-03 11:19:12 +0200

    Small settings changes

--- electronics_app/settings.py
+++ electronics_app/settings.py
@@ -126,9 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "src" / "website" / "static" / "css",
-]
+STATICFILES_DIRS = []
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
