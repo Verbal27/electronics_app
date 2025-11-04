a6d2b11126b781a9b8a4cf4735938cef14445414
Mario Nistrean <nistreanmario38@gmail.com>
2025-11-04 14:43:40 +0200

    feat: components basement & small improvements on product list & detail

---
+++ src/core/components/base.py
@@ -0,0 +1,58 @@
+from django.forms.renderers import get_default_renderer
+from django.forms.widgets import Media
+from django.utils.safestring import mark_safe
+
+
+class RenderComponentMixin:
+    request = None
+
+    def get_context(self):
+        raise NotImplementedError(
+            "Subclasses of RenderableMixin must provide a get_context() method."
+        )
+
+    def render(self, template_name=None, context=None, renderer=None):
+        renderer = renderer or get_default_renderer()
+        template = template_name or self.template_name
+        context = context or self.get_context()
+        return mark_safe(renderer.render(template, context, self.request))
+
+    def no_render(self):
+        return self.template_name, self.get_context()
+
+    @classmethod
+    def set_request(cls, request):
+        cls.request = request
+
+    __str__ = render
+    __html__ = render
+
+
+class MediaDefiningComponentClass(type):
+    """
+    Metaclass for components that can have media definitions.
+    """
+    components = []
+
+    @classmethod
+    def register_component(cls, new_class):
+        cls.components.append(new_class)
+
+    def __new__(mcs, name, bases, attrs):
+        new_class = super().__new__(mcs, name, bases, attrs)
+        definition = getattr(new_class, 'Media', None)
+        if definition:
+            new_class.media = Media(definition)
+            MediaDefiningComponentClass.register_component(new_class)
+        return new_class
+
+
+class MediaDefiningComponent(metaclass=MediaDefiningComponentClass):
+
+    @classmethod
+    def render_js(cls):
+        return ''.join(cls.media.render_js())
+
+    @classmethod
+    def render_css(cls):
+        return ''.join(cls.media.render_css())
---
+++ src/core/templatetags/base_tags.py
@@ -0,0 +1,17 @@
+from decimal import Decimal
+from django import template
+from django.utils.safestring import mark_safe
+
+from src.core.components.base import MediaDefiningComponentClass
+
+register = template.Library()
+
+
+@register.simple_tag
+def component_js():
+    return mark_safe(''.join({component.render_js() for component in MediaDefiningComponentClass.components}))
+
+
+@register.simple_tag
+def component_css():
+    return mark_safe(''.join([component.render_css() for component in MediaDefiningComponentClass.components]))
