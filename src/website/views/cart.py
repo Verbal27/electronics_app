0a2f0acdf96bf75714df06a6652364821b17facf
Verbal27 <43434781+Verbal27@users.noreply.github.com>
2025-11-04 12:27:33 +0200

    Refactoring cart view

--- src/website/views/cart.py
+++ src/website/views/cart.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from ..services import Cart
 from src.core.models import Product
+from django.views.generic import ListView
 
 
-def cart_view(request):
-    cart = Cart(request)
-    cart_items_with_products = []
-    for pid, item in cart.cart.items():
-        product = Product.objects.get(pk=pid)
-        cart_items_with_products.append(
-            {
-                "id": pid,
-                "name": item["name"],
-                "price": item["price"],
-                "quantity": item["quantity"],
-                "image": product.image,
-                "description": product.description,
-            }
-        )
+class CartListView(ListView):
+    def cart_view(self, request):
+        self.cart = Cart(request)
+        cart_items_with_products = []
+        for pid, item in self.cart.cart.items():
+            product = Product.objects.get(pk=pid)
+            cart_items_with_products.append(
+                {
+                    "id": pid,
+                    "name": item["name"],
+                    "price": item["price"],
+                    "quantity": item["quantity"],
+                    "image": product.image,
+                    "description": product.description,
+                }
+            )
 
-    context = {
-        "cart_items": cart_items_with_products,
-        "total": cart.get_total(),
-    }
-    return render(request, "cart.html", context)
+        context = {
+            "cart_items": cart_items_with_products,
+            "total": self.cart.get_total(),
+        }
+        return render(request, "cart.html", context)
 
 
 def add_to_cart(request, product_id):
