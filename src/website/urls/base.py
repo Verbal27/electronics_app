4dec27a48e089090df534e509e58f43fa7452087
Verbal27 <43434781+Verbal27@users.noreply.github.com>
2025-11-05 19:17:47 +0200

    Fix: Fixed issues mentioned in PR

--- src/website/urls/base.py
+++ src/website/urls/base.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from src.website import views
 
 urlpatterns = [
@@ -14,17 +14,28 @@ urlpatterns = [
         views.SubCategoryProductListView.as_view(),
         name="subcategory_products",
     ),
-    path("cart/", views.CartListView.as_view(), name="cart"),
-    path("cart/add/<int:product_id>/", views.CartAddView.as_view(), name="add_to_cart"),
     path(
-        "cart/remove/<int:product_id>/",
-        views.CartRemoveItemView.as_view(),
-        name="remove_from_cart",
+        "cart/",
+        include(
+            [
+                path("", views.CartListView.as_view(), name="cart"),
+                path(
+                    "add/<int:product_id>/",
+                    views.CartAddView.as_view(),
+                    name="add_to_cart",
+                ),
+                path(
+                    "remove/<int:product_id>/",
+                    views.CartRemoveItemView.as_view(),
+                    name="remove_from_cart",
+                ),
+                path(
+                    "update/<int:product_id>/",
+                    views.CartUpdateQuantityView.as_view(),
+                    name="update_cart",
+                ),
+                path("clear/", views.CartDropView.as_view(), name="clear_cart"),
+            ]
+        ),
     ),
-    path(
-        "cart/update/<int:product_id>/",
-        views.CartUpdateQuantityView.as_view(),
-        name="update_cart",
-    ),
-    path("cart/clear/", views.CartDropView.as_view(), name="clear_cart"),
 ]
