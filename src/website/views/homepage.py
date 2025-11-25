b99bee7c62153b0f8accdd4b37263247ce23f12a
Mario Nistrean <nistreanmario38@gmail.com>
2025-11-25 14:22:27 +0200

    feat: started implementing component for input

--- src/website/views/homepage.py
+++ src/website/views/homepage.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView
-from src.core.models import Product
+
+from src.core.components.website.inputs import SimpleInput
+from src.core.models import Product, Category
 from src.core.components.website.cards import ProductCard
 from src.website.forms.cart import AddToCartForm
 
@@ -14,6 +16,25 @@ class HomePageListView(ListView):
         products = context.get("products", Product.objects.all())
         context["product_cards"] = [
             ProductCard(request=self.request, product=product) for product in products
+        ]
+        context['search_bar'] = SimpleInput(
+            name='search',
+            placeholder="Search items"
+        )
+        categories_obj = Category.objects.filter()
+        context['categs'] = [
+            {
+                'category_name': categories_obj[0].name,
+                'icon': 'fas fas-computer'
+            },
+            {
+                'category_name': categories_obj[1].name,
+                'icon': 'fas fas-smartphone'
+            },
+            {
+                'category_name': categories_obj[2].name,
+                'icon': 'fas fas-smartphone'
+            },
         ]
         return context
 
